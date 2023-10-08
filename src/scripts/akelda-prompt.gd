33dfd36ae6cd997a6feec6d111cc3b4227ee7a81
Jagr Krtanjek <jagr@Jagrs-MacBook-Air.local>
2023-10-08 09:24:22 -0400

    fixed dependent assets not loading on web3

--- src/scripts/akelda-prompt.gd
+++ src/scripts/akelda-prompt.gd
@@ -5,11 +5,14 @@ var character = preload("res://src/scenes/characters/pre-industrial/akelda/akeld
 var town = preload("res://src/scenes/pre-industry.tscn")
 var bkgd = preload("res://src/scenes/Background.tscn")
 var tree = preload("res://src/scenes/Tree.tscn")
+var text = preload("res://src/scenes/Textbox.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var treeNode = tree.instantiate()
+	var textbox = text.instantiate()
+	textbox.global_position = Vector2(-304,410)
 	treeNode.set_meta("Health", 1)
 	var bkgdNode = bkgd.instantiate(1)
 	var townNode = town.instantiate()
@@ -18,6 +21,7 @@ func _ready():
 	bkgdNode.add_child(townNode)
 	bkgdNode.add_child(treeNode)
 	bkgdNode.add_child(charNode)
+	bkgdNode.add_child(textbox)
 	add_child(bkgdNode)
 	
 	
