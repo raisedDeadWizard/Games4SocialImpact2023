7942b55dfc4d50ff15722431127d1514b91788ad
Diego Jurado <diego.jurado@ieee.org>
2023-10-07 22:49:22 -0400

    Started Interaction / Fixed Book

---
+++ src/scripts/akelda-prompt.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+var akelda = preload("res://src/scenes/characters/pre-industrial/akelda/akelda_char.tscn")
+var bkgd = preload("res://src/scenes/Background.tscn")
+var tree = preload("res://src/scenes/Tree.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var treeNode = tree.instantiate()
+	treeNode.set_meta("Health", 1)
+	var bkgdNode = bkgd.instantiate(1)
+	#bkgd.set_meta("Age", 1)
+	var akeldaNode = akelda.instantiate()
+	
+	bkgdNode.add_child(treeNode)
+	bkgdNode.add_child(akeldaNode)
+	add_child(bkgdNode)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
