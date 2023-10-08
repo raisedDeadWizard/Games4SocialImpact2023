3be77ad7a5bf898a17159cade5be6d14fe022082
Diego Jurado <diego.jurado@ieee.org>
2023-10-07 23:10:26 -0400

    added walden and eumann

--- src/scripts/eumann-prompt.gd
+++ src/scripts/eumann-prompt.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-
-var akelda = preload("res://src/scenes/characters/pre-industrial/akelda/akelda_char.tscn")
+var eumann = preload("res://src/scenes/characters/pre-industrial/eumann/eumann_char.tscn")
 var bkgd = preload("res://src/scenes/Background.tscn")
 var tree = preload("res://src/scenes/Tree.tscn")
 
@@ -11,21 +10,19 @@ func _ready():
 	treeNode.set_meta("Health", 1)
 	var bkgdNode = bkgd.instantiate(1)
 	#bkgd.set_meta("Age", 1)
-	var akeldaNode = akelda.instantiate()
+	var eumannNode = eumann.instantiate()
 	
 	bkgdNode.add_child(treeNode)
-	bkgdNode.add_child(akeldaNode)
+	bkgdNode.add_child(eumannNode)
 	add_child(bkgdNode)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		print(event)
 		if event.is_pressed():
-			get_tree().change_scene_to_file("res://src/scenes/pre-industrial/eumann-prompt.tscn")
+			get_tree().change_scene_to_file("res://src/scenes/pre-industrial/walden-prompt.tscn")
 		# move to next nod
-
--- src/scripts/walden-prompt.gd
+++ src/scripts/walden-prompt.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-
-var akelda = preload("res://src/scenes/characters/pre-industrial/akelda/akelda_char.tscn")
+var walden = preload("res://src/scenes/characters/pre-industrial/walden/walden_char.tscn")
 var bkgd = preload("res://src/scenes/Background.tscn")
 var tree = preload("res://src/scenes/Tree.tscn")
 
@@ -11,21 +10,13 @@ func _ready():
 	treeNode.set_meta("Health", 1)
 	var bkgdNode = bkgd.instantiate(1)
 	#bkgd.set_meta("Age", 1)
-	var akeldaNode = akelda.instantiate()
+	var waldenNode = walden.instantiate()
 	
 	bkgdNode.add_child(treeNode)
-	bkgdNode.add_child(akeldaNode)
+	bkgdNode.add_child(waldenNode)
 	add_child(bkgdNode)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		print(event)
-		if event.is_pressed():
-			get_tree().change_scene_to_file("res://src/scenes/pre-industrial/eumann-prompt.tscn")
-		# move to next nod
 
