5e96fc9e6af522bb8cb7c02f97b55fa03f8d99ba
Jagr Krtanjek <jagr@Jagrs-MacBook-Air.local>
2023-10-08 10:29:26 -0400

    initial dialogue

--- src/scripts/TreeExpo.gd
+++ src/scripts/TreeExpo.gd
@@ -5,10 +5,8 @@ var bookNode = preload("res://src/scenes/book.tscn")
 func _ready():
 	
 	var book_instance = bookNode.instantiate()
-	book_instance.set_meta("Page1", "Tree Expo")
-	book_instance.set_meta("Page2", "Page 2!")
-	book_instance.set_meta("Page3", "Page 3!")
-	book_instance.set_meta("Page4", "Page 4!")
+	book_instance.set_meta("Page1", "The Tree of Life is fueled by the growth of the environment around it, as decay and death approaches, the Tree suffers as a result. But, as the village expands and grows so does the Tree of Life.")
+	book_instance.set_meta("Page2", "You will need to improve the lives of crucial villagers in order to keep the tree alive and make necessary changes in the history of the village. ")
 	self.add_child(book_instance)
 
 
