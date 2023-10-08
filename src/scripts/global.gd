d3326282b0df3e87401e86ad8dafd197fc3a1403
Diego Jurado <diego.jurado@ieee.org>
2023-10-08 02:31:44 -0400

    added total score

--- src/scripts/global.gd
+++ src/scripts/global.gd
@@ -3,6 +3,9 @@ extends Node
 var num_books = 9
 var is_minigame_ready = false
 var highest_book = 1000
+
+var totalScoreCurrAct = 0
+var totalScore = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
