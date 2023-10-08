4f91a1523f95dadc6947f93b91c33af01fa8a930
rlett <ral109@pitt.edu>
2023-10-08 08:14:13 -0400

    DIFFICLUTY

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -5,9 +5,24 @@ extends Node
 func _ready():
 	
 	Global.scene_index += 1
-	if Global.scene_index == 4:
-		Global.max_drops = 25
-	print("index: ",Global.scene_index)
+	print(Global.totalScore)
+	match Global.scene_index:
+		0:
+			print("UPDATED GLOBALS")
+			Global.max_drops = 25
+			Global.weed_timer = 35
+			Global.book_timer = 15
+		13:
+			print("UPDATED GLOBALS")
+			Global.max_drops = 30
+			Global.weed_timer = 25
+			Global.book_timer = 10
+		25:
+			print("UPDATED GLOBALS")
+			Global.max_drops = 35
+			Global.weed_timer = 20
+			Global.book_timer = 7
+		
 	get_tree().change_scene_to_file(Global.scene_array[Global.scene_index])
 
 
--- src/scripts/stackable_book.gd
+++ src/scripts/stackable_book.gd
@@ -43,21 +43,16 @@ func _ready():
 func _input(event):
 	#print("event")
 	if event is InputEventMouseButton:
-		print("mouse button")
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("left")
 			if _is_within_rect(get_global_mouse_position(),collisionNode.get_shape().get_rect()):
 				if Global.is_minigame_ready:
 					emit_signal("clicked",self)
-					print("CLICKED")
 			
 func _is_within_rect(m, rect):
 	var x = collisionNode.global_position.x + rect.position.x
 	var y = collisionNode.global_position.y + rect.position.y
 	var width = rect.size.x
 	var height = rect.size.y
-	print("x: ",x," y: ",y)
-	print("mouse: ", m)
 	
 	if (m.x < x or m.y < y or m.x > x+width or m.y > y+height):
 		return false
@@ -73,10 +68,8 @@ func pickup():
 	if held:
 		return
 	held = true
-	print("pickup")
 
 func drop(impulse=Vector2.ZERO):
 	if held:
 		apply_central_impulse(impulse)
 		held = false
-		print("drop")
